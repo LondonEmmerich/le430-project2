37c82c6cd00d9ec84ca9456812b2d71a2909fa10
London Emmerich <lce1549@g.rit.edu>
2023-04-18 23:53:35 -0400

    login works

--- client/login.jsx
+++ client/login.jsx
@@ -1,21 +1,98 @@
+const helper = require("./helper.js");
 const React = require("react");
 const ReactDOM = require("react-dom");
 
-const LoginWindow = (props) => {
-  const [username, setUsername] = React.useState(props.username);
+const handleLogin = (e) => {
+    e.preventDefault();
 
-  return (
-    <div>
-      <p>Hello {username}</p>
-      <label>Change Name: </label>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-    </div>
-  )
+    const username = e.target.querySelector("#user").value;
+    const pass = e.target.querySelector("#pass").value;
+
+    if(!username || !pass){
+        helper.handleError("Username or password is empty!");
+        return false;
+    }
+
+    helper.sendPost(e.target.action, {username, pass});
+
+    return false;
 }
 
+const handleSignup = (e) => {
+    e.preventDefault();
+
+    const username = e.target.querySelector("#user").value;
+    const pass = e.target.querySelector("#pass").value;
+    const pass2 = e.target.querySelector("#pass2").value;
+
+    if(!username || !pass || !pass2) {
+        helper.handleError("Passwords do not match!");
+        return false;
+    }
+
+    helper.sendPost(e.target.action, {username, pass, pass2});
+
+    return false;
+};
+
+const LoginWindow = (props) => {
+    return(
+        <form id="loginForm"
+        name="loginForm"
+        onSubmit={handleLogin}
+        action="/login"
+        method="POST"
+        className="mainForm"
+        >
+            <label htmlFor="username">Username: </label>
+            <input id="user" type="text" name="username" placeholder="username"/>
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password"/>
+            <input className="formSubmit" type="submit" value="Sign in"/>
+        </form>
+    );
+};
+
+const SignupWindow = (props) => {
+    return(
+        <form id="signupForm"
+        name="signupForm"
+        onSubmit={handleSignup}
+        action="/signup"
+        method="POST"
+        className="mainForm"
+        >
+            <label htmlFor="username">Username: </label>
+            <input id="user" type="text" name="username" placeholder="username"/>
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password"/>
+            <label htmlFor="pass">Password: </label>
+            <input id="pass2" type="password" name="pass2" placeholder="retype password"/>
+            <input className="formSubmit" type="submit" value="Sign up"/>
+        </form>
+    );
+};
+
 const init = () => {
-  ReactDOM.render( <LoginWindow />,
-    document.getElementById('app'));
+    const loginButton = document.getElementById("loginButton");
+    const signupButton = document.getElementById("signupButton");
+
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        ReactDOM.render(<LoginWindow/>,
+            document.getElementById("content"));
+        return false;
+    });
+
+    signupButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        ReactDOM.render(<SignupWindow/>,
+            document.getElementById("content"));
+        return false;
+    });
+
+    ReactDOM.render(<LoginWindow/>,
+        document.getElementById("content"));
 };
 
 window.onload = init;
